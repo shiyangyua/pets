29dab53daf1e94881b5bca74205f727d4f473075
YangYu <elementui@foxmail.com>
2020-08-16 00:32:35 +0800

    picker_edit

--- web/src/pages/mine/mineEdit.vue
+++ web/src/pages/mine/mineEdit.vue
@@ -1,32 +1,38 @@
 <template>
 	<view>
-		<view class=" mineEdit">
-			<image src="../../static/headpic.jpg" mode=""></image>
-		</view>
-		<form @submit="" @reset="">
-			<uni-list>
-				<uni-list-item title="昵称">
-				</uni-list-item>
-				<uni-list-item title="性别">
-				</uni-list-item>
-				<uni-list-item title="城市">
-					<view>			<!-- 地区选择器 -->
-						<view class="uni-list-cell-db">
-							<picker @change="bindPickerChange" :value="index" :range="array">
-								<view class="uni-input">{{array[index]}}</view>
-							</picker>
-						</view>
+		<form @submit="" @reset=""class="editForm">
+			<view class=" mineEdit">
+				<image src="../../static/headpic.jpg" mode=""></image>
+			</view>
+			<view class="formItem">
+				<view class="itemLabel">
+					昵称：
+				</view> <input type="text" value="oxygen" />
+			</view>
+			<view class="formItem">
+				<view class="itemLabel">性别：</view>
+				<view class="itemMain">
+					<picker @change="bindPickerChange" :value="sexIndex" :range="sex">
+						<view class="uni-input">{{sex[index]}}</view>
+					</picker>
+				</view>
+			</view>
+			<view class="formItem">
+				<view class="itemLabel"> 城市： </view>
+					<view class="itemMain">
+						<picker @change="bindPickerChange" :value="index" :range="array">
+							<view class="uni-input">{{array[index]}}</view>
+						</picker>
 					</view>
-				</uni-list-item>
-				<uni-list-item title="自我介绍"> </uni-list-item>
-
-			</uni-list>
+			</view>
+			<view  class="formItem">
+				自我介绍：
+			</view> <textarea value="" placeholder="" />
 			<button form-type="submit" type="default">保存</button>
 			<button form-type="reset">Reset</button>
 		</form>
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
@@ -34,10 +40,11 @@
 				title: 'picker',
 				array: ['中国', '美国', '巴西', '日本'],
 				index: 0,
+				sex:['男', '女','未知'],
+				sexIndex: 0,
 			}
 		},
 		methods: {
-			selectPosition() {},
 			bindPickerChange: function(e) {
 				console.log('picker发送选择改变，携带值为', e.target.value)
 				this.index = e.target.value
@@ -47,11 +54,11 @@
 </script>
 
 <style lang="scss" scoped>
+.editForm{
 	.mineEdit {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-
 		image {
 			width: 150rpx;
 			height: 150rpx;
@@ -59,11 +66,23 @@
 			// margin: 10rpx auto;
 		}
 	}
-uni-list{
-	uni-list-item{
-		width: 100%;
+	.formItem{
 		display: flex;
-		// flex-direction: ;
+		margin: 30rpx 0 25rpx 30rpx;
+		border-bottom: 1rpx solid #CCCCCC;
+		.itemLabel{
+			margin: 0rpx 20rpx 0rpx 0rpx;
+			color: #CCCCCC;
+		}
+		.itemMain{
+			
+		}
+		// textarea{
+		// 	border: 1px solid #CCCCCC;
+		// 	margin: 5rpx 10rpx 20rpx 5rpx;
+		// 	height: 100rpx;
+		// 	width: 90%;
+		// }
 	}
 }
 </style>
